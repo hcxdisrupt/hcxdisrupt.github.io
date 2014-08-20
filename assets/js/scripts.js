43178d7c06b82b84fc18c736a601cdd6d9b18f51
asronline <Ammaar@ammaars-air.home>
2014-08-20 21:42:59 +0100

    fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -136,23 +136,3 @@ $(document).ready( function(){
 
 });
 
-
-/**	7. GOOGLE ANALYTICS
- *****************************************************/
-$(document).ready( function(){
-	
-	var google_analytics_id = 'UA-48390919-1';  // Enter your ID here. like this: UA-2121212-22
-		  
-		  if(google_analytics_id != '') {
-			  var _gaq = _gaq || [];
-			  _gaq.push(['_setAccount', google_analytics_id]);
-			  _gaq.push(['_trackPageview']);
-			  
-			  (function() {
-			  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			  })();
-		  }	
-		  
-});
